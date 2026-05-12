700133b87e56585db6ab986bbe95f7a24ce52d37
Vimal Kumar .K.J <76762337+VimalKumarKJ@users.noreply.github.com>
2026-05-12 20:21:31 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.py
@@ -0,0 +1,26 @@
+import heapq
+from collections import Counter
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        task_frequency = Counter(tasks)
+        counter = 0
+        tasks = [(-val, key) for key, val in task_frequency.items()]
+        heapq.heapify(tasks)
+
+        while tasks:
+            temp_tasks_tracker = []
+
+            for _ in range(n+1):
+                if not tasks and not temp_tasks_tracker:
+                    break
+                counter += 1
+                if tasks:
+                    curr_task = heapq.heappop(tasks)
+
+                    if curr_task[0] + 1 < 0:
+                        temp_tasks_tracker.append((curr_task[0]+1, curr_task[1]))
+                    
+            for temp_tasks in temp_tasks_tracker:
+                heapq.heappush(tasks, temp_tasks)
+        return counter
